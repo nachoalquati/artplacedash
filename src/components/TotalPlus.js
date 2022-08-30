a89e9b0609488edf4a72199ec078fc192f482dca
nachoalquati <nachoalquati@gmail.com>
2022-08-30 19:02:22 -0300

    Agrego styles

--- src/components/TotalPlus.js
+++ src/components/TotalPlus.js
@@ -6,14 +6,20 @@ import ContentRowMovies from './ContentRowMovies';
 function ContentRowCenter(){
     let spacing = '15px'
     return (
-        <div className="row"  style={{marginLeft: spacing}}>
+        <div className="row position-relative"  style={{marginLeft: spacing}}>
             
             {/*<!-- Last Movie in DB -->*/}
+            <div className='top-50 start-50 translate-middle' style={{margin:   'auto'}}>
             <ContentRowMovies />
+
+            </div>
             {/*<!-- End content row last movie in Data Base -->*/}
 
             {/*<!-- Genres in DB -->*/}
+            <div className='col-12'>
             <Chart />
+
+            </div>
 
         </div>
     )
